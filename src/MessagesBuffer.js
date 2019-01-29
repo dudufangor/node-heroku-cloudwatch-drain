5ecf42fe007ed62aa829e9cc1d7869fbbfc7b8f2
dudufangor <dudufangor@gmail.com>
2019-01-29 20:21:11 -0200

    Fix

--- src/MessagesBuffer.js
+++ src/MessagesBuffer.js
@@ -93,7 +93,7 @@ class MessagesBuffer {
     for (let message of this.messages) {
       sizeEstimate += 26 + this.getMessageSize(message);
 
-      if (sizeEstimate > 1000000 || batchCount >= this.batchSize) {
+      if (sizeEstimate >= 900000 || batchCount >= this.batchSize) {
         break;
       } else {
         batchCount += 1;
